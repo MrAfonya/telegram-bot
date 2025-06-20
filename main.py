45a598309ecbf5474176035e833b806c59641ddb
MrAfonya <artema007@gmail.com>
2025-06-20 11:32:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import telebot
 
+OWNER_ID = 784479521
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    if message.from_user.id != OWNER_ID:
+        bot.reply_to(message, "Извините, доступ только для владельца.")
+        return
+    bot.reply_to(message, f"Ты написал: {message.text}")
+
 API_TOKEN = '8091220315:AAHDf1LMI5UeuDzdLyAANjAsirmrOlu8UIg'
 
 bot = telebot.TeleBot(API_TOKEN)
